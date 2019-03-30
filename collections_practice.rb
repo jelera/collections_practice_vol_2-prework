99bcd38a1def67ec2c391138f48bc471f03d74c7
Jose Elera <jelera@gmail.com>
2019-03-30 03:11:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,14 +3,13 @@
 def begins_with_r(words)
   letter = "r"
 
-  output = false
+  output = true
 
   words.each do |word|
     if !word.start_with?(letter)
       return false
-    else
-      output = true
     end
   end
-  output
+  
+  output 
 end
